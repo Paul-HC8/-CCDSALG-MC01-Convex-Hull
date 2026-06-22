ea1c4c15b3e3d7c4c2f64960825f32a83b4b2fd1
zapepper <122518042+zapepper@users.noreply.github.com>
2026-06-23 00:55:18 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -4,8 +4,8 @@
 
     Group #: 04  (2 digits)
 
-    CHENG, DERK SIMONE C. S04
-    CORTEZ, PAUL HAVEN C. S09
+    CHENG, DERK SIMONE C.  S04
+    CORTEZ, PAUL HAVEN C.  S09
     SORILLA, JOSHUA DEL F. S09
     
 
